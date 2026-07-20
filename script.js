7150e57383dcc104a8070805318f511e0b5c1ece
Hannabellz <hannah.klossgleason@gmail.com>
2026-07-19 22:07:27 -0400

    initial push

---
+++ script.js
@@ -0,0 +1,14 @@
+function normalizeTime() {
+    const currentTime = new Date();
+    const hour = currentTime.getHours(); //0-23 value
+    const convert = (hour * 27) / 23;
+    var time = "norm";
+    if (convert == 19) {
+        time = "dusk";          
+    }
+    document.cookie = "time=" + encodeURIComponent(time) +
+        "; max-age=3600" +
+        "; path=/" +
+        "; Secure" +
+        "; SameSite=Lax";
+}
